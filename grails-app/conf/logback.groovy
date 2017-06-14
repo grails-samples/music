109f0224da4cdceb2e711def2c078d5b552bfd17
Jeff Scott Brown <brownj@objectcomputing.com>
2017-06-14 15:14:11 -0500

    Turn up some logging

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -34,3 +34,5 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
 root(ERROR, ['STDOUT'])
+
+logger 'music.ItunesSearchService', DEBUG, ['STDOUT'], false
